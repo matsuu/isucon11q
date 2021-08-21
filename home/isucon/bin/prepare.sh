efb7642c4cfeb29d12e5bb95f6647bbdf81a22d2
ウー馬場ーイー2 <ubabar-eat2@isucon.net>
2021-08-21 14:47:03 +0900

    146710

--- home/isucon/bin/prepare.sh
+++ home/isucon/bin/prepare.sh
@@ -3,8 +3,8 @@ set -ex
 # mysql
 #sudo mysql -e "CALL sys.ps_truncate_all_tables(FALSE)"
 #sudo mysql -e "TRUNCATE TABLE performance_schema.events_statements_summary_by_digest"
-sudo rm -f /var/lib/mysql/mysql-slow.log
-sudo mysqladmin flush-slow-log
+sudo ssh 192.168.0.12 rm -f /var/lib/mysql/mysql-slow.log
+sudo ssh 192.168.0.12 mysqladmin flush-slow-log
 # app
 sudo systemctl restart jiaapi-mock.service
 sudo systemctl restart isucondition.go.service
